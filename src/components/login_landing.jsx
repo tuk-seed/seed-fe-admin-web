eb8c4325165f5dc700b3a556130c896452aed5a5
Lawrence75g <osurulawrence@gmail.com>
2023-07-24 20:55:37 +0300

    login_landing

--- src/components/login_landing.jsx
+++ src/components/login_landing.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { ResponsiveWrapper } from '../hoc';
 
-function login_landing() {
+import React from 'react';
+import { ResponsiveWrapper } from '../hoc';
+import LogoImage from './img/logo.png';
+
+function LoginLanding() {
   return (
-    <h1 className="text-3xl font-bold">
-      login_landing
-    </h1>
-  )
+
+    <div className="bg-teal-900  h-screen flex items-center justify-center">
+      <div className="rounded-full h-20 w-20 border-blue-500"></div>
+      <img
+        src={LogoImage}
+        alt="Logo"
+        className="h-159 w-303 ml-4 sm:w-303px sm:h-159px sm:ml-0 sm:rounded-10"
+      />
+    </div>
+
+  );
 }
 
-export default ResponsiveWrapper(login_landing);
+export default ResponsiveWrapper(LoginLanding);
