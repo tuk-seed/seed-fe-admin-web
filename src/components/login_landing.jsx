f5e8bf6f4c4363f161d7103f59cc450c100d1131
Lawrence75g <osurulawrence@gmail.com>
2023-07-26 14:55:00 +0300

    login_landing page

--- src/components/login_landing.jsx
+++ src/components/login_landing.jsx
@@ -1,20 +1,40 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ResponsiveWrapper } from '../hoc';
-import LogoImage from './img/logo.png';
+// import LogoImage from './img/logo.png';
+
+// Spinner Component
+function Spinner() {
+  return (
+    <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+  );
+}
 
 function LoginLanding() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating a delay to demonstrate the loading effect
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    // Clear the timer on component unmount
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-
-    <div className="bg-teal-900  h-screen flex items-center justify-center">
-      <div className="rounded-full h-20 w-20 border-blue-500"></div>
-      <img
-        src={LogoImage}
-        alt="Logo"
-        className="h-159 w-303 ml-4 sm:w-303px sm:h-159px sm:ml-0 sm:rounded-10"
-      />
+    <div style={{ background: '#2A4454' }} className="h-screen flex items-center justify-center">
+      {loading ? (
+        <Spinner />
+      ) : (
+        <img
+          src="./assets/Logoo.png"
+          alt="Logo"
+          className="h-159 w-303 ml-4 sm:w-303px sm:h-159px sm:ml-0 sm:rounded-10"
+        />
+      )}
     </div>
-
   );
 }
 
